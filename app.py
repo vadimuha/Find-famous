8810106ace74774bf6fe64b40c1f859bb81c3624
vadimuha <vadimuha13@gmail.com>
2017-10-17 09:57:38 +0300

    junk names solved

--- app.py
+++ app.py
@@ -13,23 +13,31 @@ def index():
 	return render_template("index.html")
 
 def select_type(table,column_name,search_by,query):
-	db.execute("SELECT {} FROM {} WHERE {} LIKE ? GROUP BY {}".format(column_name,table,search_by,column_name),[('%{}%'.format(query))])
+	db.execute("SELECT DISTINCT {} FROM {} WHERE {} LIKE ? GROUP BY {}".format(column_name,table,search_by,column_name),[('%{}%'.format(query))])
 	result = db.fetchall()
 	return result
-def select_all_actors(query):
-	db.execute("SELECT actor_1_name,actor_2_name,actor_3_name FROM data WHERE actor_1_name LIKE ? OR actor_2_name LIKE ? OR actor_3_name LIKE ? GROUP BY actor_1_name,actor_2_name,actor_3_name",(["%{}%".format(query),"%{}%".format(query),"%{}%".format(query)]))
-	result = db.fetchall()
-	return result
+# def select_all_actors(query):
+# 	db.execute("SELECT actor_1_name,actor_2_name,actor_3_name FROM data WHERE actor_1_name LIKE ? OR actor_2_name LIKE ? OR actor_3_name LIKE ? GROUP BY actor_1_name,actor_2_name,actor_3_name",(["%{}%".format(query),"%{}%".format(query),"%{}%".format(query)]))
+# 	result = db.fetchall()
+# 	return result
 
 @app.route("/get_acrot")
 def get_actor():
 	if request.method == "GET":
 		name = request.args.get("q")
+
 		directors = select_type("data","director_name","director_name",name)
-		actors = select_all_actors(name)
+
+		actors  = select_type("data","actor_1_name","actor_1_name",name)
+		actors += select_type("data","actor_2_name","actor_2_name",name)
+		actors += select_type("data","actor_3_name","actor_3_name",name)
+		
 		nobel = select_type("nobel","firstname,surname,category,motivation,name","firstname",name)
+		
 		json = [0]*3
 		json[0] = directors
 		json[1] = actors
 		json[2] = nobel
 		return jsonify(json)
+if __name__ == "__main__":
+    app.run(port=80)
